1d2cf0d2abfe82f3a966756f9a3f282c47247c27
fl4shk <fl4shk@users.noreply.github.com>
2018-09-21 09:21:41 -0400

    Stuffs

---
+++ hdl/src/snow64_operand_manager_defines.header.sv
@@ -0,0 +1,8 @@
+`ifndef src__slash__snow64_operand_manager_defines_header_sv
+`define src__slash__snow64_operand_manager_defines_header_sv
+
+// src/snow64_operand_manager_defines.header.sv
+
+`include "src/misc_defines.header.sv"
+
+`endif		// src__slash__snow64_operand_manager_defines_header_sv
