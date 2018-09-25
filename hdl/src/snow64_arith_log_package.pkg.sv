545c2bf4d065f13c9e9de49a35be5e83e01f47c2
fl4shk <fl4shk@users.noreply.github.com>
2018-09-25 19:38:33 -0400

    Going to use 64-bit ALU instead of carry-break ALU

--- hdl/src/snow64_arith_log_package.pkg.sv
+++ hdl/src/snow64_arith_log_package.pkg.sv
@@ -27,36 +27,36 @@ typedef enum logic [`MSB_POS__SNOW64_ALU_OPER:0]
 	OpDummy4
 } AluOper;
 
-localparam __DEBUG_ENUM__OP_ADD = OpAdd;
-localparam __DEBUG_ENUM__OP_SUB = OpSub;
-localparam __DEBUG_ENUM__OP_SLT = OpSlt;
-localparam __DEBUG_ENUM__OP_DUMMY_0 = OpDummy0;
-localparam __DEBUG_ENUM__OP_DUMMY_1 = OpDummy1;
-localparam __DEBUG_ENUM__OP_AND = OpAnd;
-localparam __DEBUG_ENUM__OP_ORR = OpOrr;
-localparam __DEBUG_ENUM__OP_XOR = OpXor;
-localparam __DEBUG_ENUM__OP_SHL = OpShl;
-localparam __DEBUG_ENUM__OP_SHR = OpShr;
-localparam __DEBUG_ENUM__OP_INV = OpInv;
-localparam __DEBUG_ENUM__OP_NOT = OpNot;
-localparam __DEBUG_ENUM__OP_ADD_AGAIN = OpAddAgain;
-localparam __DEBUG_ENUM__OP_DUMMY_2 = OpDummy2;
-localparam __DEBUG_ENUM__OP_DUMMY_3 = OpDummy3;
-localparam __DEBUG_ENUM__OP_DUMMY_4 = OpDummy4;
+//localparam __DEBUG_ENUM__OP_ADD = OpAdd;
+//localparam __DEBUG_ENUM__OP_SUB = OpSub;
+//localparam __DEBUG_ENUM__OP_SLT = OpSlt;
+//localparam __DEBUG_ENUM__OP_DUMMY_0 = OpDummy0;
+//localparam __DEBUG_ENUM__OP_DUMMY_1 = OpDummy1;
+//localparam __DEBUG_ENUM__OP_AND = OpAnd;
+//localparam __DEBUG_ENUM__OP_ORR = OpOrr;
+//localparam __DEBUG_ENUM__OP_XOR = OpXor;
+//localparam __DEBUG_ENUM__OP_SHL = OpShl;
+//localparam __DEBUG_ENUM__OP_SHR = OpShr;
+//localparam __DEBUG_ENUM__OP_INV = OpInv;
+//localparam __DEBUG_ENUM__OP_NOT = OpNot;
+//localparam __DEBUG_ENUM__OP_ADD_AGAIN = OpAddAgain;
+//localparam __DEBUG_ENUM__OP_DUMMY_2 = OpDummy2;
+//localparam __DEBUG_ENUM__OP_DUMMY_3 = OpDummy3;
+//localparam __DEBUG_ENUM__OP_DUMMY_4 = OpDummy4;
+//
+//
+//localparam __DEBUG_PORT_MSB_POS__DATA_INOUT
+//	= `MSB_POS__SNOW64_SIZE_64;
+//localparam __DEBUG_PORT_MSB_POS__OPER = `MSB_POS__SNOW64_ALU_OPER;
+//localparam __DEBUG_PORT_MSB_POS__INT_TYPE_SIZE
+//	= `MSB_POS__SNOW64_CPU_INT_TYPE_SIZE;
 
-
-localparam __DEBUG_PORT_MSB_POS__DATA_INOUT
-	= `MSB_POS__SNOW64_SIZE_64;
-localparam __DEBUG_PORT_MSB_POS__OPER = `MSB_POS__SNOW64_ALU_OPER;
-localparam __DEBUG_PORT_MSB_POS__INT_TYPE_SIZE
-	= `MSB_POS__SNOW64_CPU_INT_TYPE_SIZE;
-
-typedef struct packed
-{
-	logic [`MSB_POS__SNOW64_SIZE_64:0] a, b;
-	logic [`MSB_POS__SNOW64_ALU_OPER:0] oper;
-	logic type_signedness;
-} PortIn_SubAlu;
+//typedef struct packed
+//{
+//	logic [`MSB_POS__SNOW64_SIZE_64:0] a, b;
+//	logic [`MSB_POS__SNOW64_ALU_OPER:0] oper;
+//	logic type_signedness;
+//} PortIn_SubAlu;
 
 
 
@@ -108,24 +108,17 @@ typedef struct packed
 //	logic carry, lts;
 //} PortOut_SubAlu;
 
-//typedef struct packed
-//{
-//	logic [`MSB_POS__SNOW64_SUB_ALU_DATA_INOUT:0] a, b;
-//	logic carry;
-//	logic [`MSB_POS__SNOW64_ALU_OPER:0] oper;
-//	logic [`MSB_POS__SNOW64_CPU_INT_TYPE_SIZE:0] int_type_size;
-//	logic [`MSB_POS__SNOW64_SUB_ALU_INDEX:0] index;
-//} PortIn_SubAlu;
-//
-//typedef struct packed
-//{
-//	logic [`MSB_POS__SNOW64_SIZE_8:0] data;
-//
-//	// Note that "carry" also is equivalent to the "sltu" result.
-//	logic carry, lts;
-//} PortOut_SubAlu;
+typedef struct packed
+{
+	logic [`MSB_POS__SNOW64_SIZE_64:0] a, b;
+	logic carry;
+} PortIn_SubAlu;
 
-
+typedef struct packed
+{
+	logic [`MSB_POS__SNOW64_SIZE_64:0] data;
+	logic [`MSB_POS__SNOW64_SUB_ALU_OUT_SLT:0] slt;
+} PortOut_SubAlu;
 
 
 localparam WIDTH__OF_64 = 64;
