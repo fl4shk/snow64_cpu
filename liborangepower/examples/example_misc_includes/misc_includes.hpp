5de8af6da476a29432a16a5395bd86739b8f347d
fl4shk <fl4shk@users.noreply.github.com>
2018-10-11 20:17:07 -0400

    git subrepo pull liborangepower
    subrepo:
      subdir:   "liborangepower"
      merged:   "1fc6288"
    upstream:
      origin:   "https://github.com/fl4shk/liborangepower.git"
      branch:   "master"
      commit:   "1fc6288"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a7ee886"

--- liborangepower/examples/example_misc_includes/misc_includes.hpp
+++ liborangepower/examples/example_misc_includes/misc_includes.hpp
@@ -2,6 +2,7 @@
 #define src__slash__misc_includes_hpp
 
 
+// src/misc_includes.hpp
 
 
 #include "liborangepower_src/misc_includes.hpp"
@@ -11,11 +12,13 @@
 #include "liborangepower_src/misc_bitwise_funcs.hpp"
 #include "liborangepower_src/misc_defines.hpp"
 #include "liborangepower_src/gen_getter_setter_defines.hpp"
+#include "liborangepower_src/string_conversion_stuff.hpp"
 
 using namespace liborangepower::printout_etc;
 using namespace liborangepower::integer_types;
 using namespace liborangepower::misc_util;
 using namespace liborangepower::bitwise;
+using namespace liborangepower::strings;
 
 
 
